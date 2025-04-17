48de025c0d3164ec2f24f25f0bc8a6d67b8027fa
Albert Taylor <42644856+Tasumin@users.noreply.github.com>
2025-04-17 14:16:34 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 export default function handler(req, res) {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const path = url.pathname.toLowerCase();
-
+  
   const redirects = {
     '/servicedesk': 'http://renpsg.atlassian.net/servicedesk/customer/portal/2',
     '/changerequest': 'http://renpsg.atlassian.net/servicedesk/customer/portal/5',
     '/google': 'https://google.com',
     '/myapps': 'https://myapplications.office.com'
   };
-
   if (redirects[path]) {
     res.writeHead(308, { Location: redirects[path] });
     res.end();
@@ -17,3 +16,4 @@ export default function handler(req, res) {
     res.end('Not Found');
   }
 }
+
